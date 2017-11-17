32eed9cab830498a9d36f982465d6eaf4aafb92d
Unknown <kevin.pinzon@ulbra.br>
2017-11-17 08:46:07 -0200

    object renamed to knapsack-object

---
+++ knapsack-object.js
@@ -0,0 +1,8 @@
+function KnapsackObject(value = 0, weight = 0){
+    return {
+        value,
+        weight
+    }
+}
+
+module.exports = KnapsackObject;
--- object.js
+++
@@ -1,8 +0,0 @@
-function Object(value = 0, weight = 0){
-    return {
-        value,
-        weight
-    }
-}
-
-module.exports = Object;
