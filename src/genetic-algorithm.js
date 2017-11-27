6aed526aaab26e35973d1c532a0c21889b918f24
Kevin <kevin.pinzon@ulbra.br>
2017-11-27 17:44:24 -0200

    removed evolve

--- src/genetic-algorithm.js
+++ src/genetic-algorithm.js
@@ -63,36 +63,6 @@ class GeneticAlgorithm {
         
         return newParents;
     }
-    evolve() {
-        let numberOfIndividualsToSubstitute = (Math.round(this.algorithmParams.populationSize * this.algorithmParams.generationInterval / 100));
-        const quantityToSubstitueIsOdd = (numberOfIndividualsToSubstitute % 2 !== 0);
-        let parents;
-        let pairedParents;
-
-        parents = this.solution.selection(
-            this.population,
-            (quantityToSubstitueIsOdd ? ++numberOfIndividualsToSubstitute : numberOfIndividualsToSubstitute)
-        );
-
-        pairedParents = parents.reduce((pairs, parent, index) => {
-            const firstParent = 0;
-            const secondParent = 1;
-            let pairIndex = Math.floor(index / 2);
-            let parentIndex = (index % 2 === 0) ? firstParent : secondParent;
-
-            if (!pairs[pairIndex]) pairs[pairIndex] = [];
-            pairs[pairIndex][parentIndex] = parent;
-            
-            return pairs;
-        }, (new Array(numberOfIndividualsToSubstitute / 2)));
-
-        pairedParents.forEach(pair => {
-            const doCrossover = this.utils.shouldDoSomething(this.algorithmParams.crossoverRate);
-
-            if (doCrossover)
-             this.solution.crossover(pair[0], pair[1]);
-        });
-    }
 
     getSolution() {
 
