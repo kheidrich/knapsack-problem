a7ad8e3dd6643d193878f4e20a794c45e9065aca
Kevin <kevin.pinzon@ulbra.br>
2017-12-07 13:26:05 -0200

    removed crossoverRate parameter

--- src/core/algorithm-parameters.js
+++ src/core/algorithm-parameters.js
@@ -2,13 +2,11 @@ class AlgorithmParameters {
     constructor(
         populationSize,
         mutationRate,
-        crossoverRate,
         generationInterval
     ) {
         this.populationSize = populationSize;
         this.generationInterval = generationInterval;
         this.mutationRate = mutationRate;
-        this.crossoverRate = crossoverRate;
     }
 }
 
