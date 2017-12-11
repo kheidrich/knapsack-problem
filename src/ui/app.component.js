06f38b464bfd81285c34d1c5e1b17330023ff177
Unknown <kevin.pinzon@ulbra.br>
2017-12-10 22:00:20 -0200

    trial and error

--- src/ui/app.component.js
+++ src/ui/app.component.js
@@ -1,12 +1,46 @@
 import template from './app.component.html';
+import KnapsackGaSolution from '../core/knapsack-ga-solution';
+import GeneticAlgorithm from '../core/genetic-algorithm';
 
 class AppComponent {
-    constructor(){
-
+    constructor(ModalService) {
+        this.ModalService = ModalService;
+        this.solutionStatus = 'configuring';
     }
 
-    updateParameters(){
-        console.log(this.knapsackParameters)
+    updateParameters() {
+        console.log(this.geneticParameters);
+    }
+
+    startSolution() {
+        const { populationSize, generationInterval, mutationRate } = this.geneticParameters;
+        const { geneMutationRate } = this.geneticParameters;
+        let solution = new KnapsackGaSolution(this.knapsackParameters, { geneMutationRate });
+        let ga = new GeneticAlgorithm(solution, { populationSize, generationInterval, mutationRate });
+
+
+        this.solutionStatus = 'solving';
+        ga.initialize();
+        this.initialPopulation = [...ga.population];
+        this.ModalService.openModal('comp');
+
+        let iterations = 0;
+        let population;
+        let parents;
+
+        while (iterations < 100) {
+            ga.updateOptimal();
+            parents = ga.selectParents();
+            parents = ga.crossover([...parents]);
+            parents = ga.mutate([...parents]);
+            ga.substitute(parents);
+            iterations++;
+        }
+
+        this.finalPopulation = [...ga.population];
+        this.optimalHistory = [...ga.optimalHistory];
+        this.ModalService.closeModal('comp');
+        this.solutionStatus = 'solved';
     }
 }
 
