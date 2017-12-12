8b05e229e36157cca850f0f7d4df11147d3107b9
Unknown <kevin.pinzon@ulbra.br>
2017-12-12 01:19:09 -0200

    removed knapsack-summary

--- src/ui/solution-info/solution-info.module.js
+++ src/ui/solution-info/solution-info.module.js
@@ -2,13 +2,10 @@ import angular from 'angular';
 
 import ElementsModule from '../elements/elements.module';
 
-import KnapsackSummaryComponent from './knapsack-summary/knapsack-summary.component';
 import PopulationDetailsComponent from './population-details/population-details.component';
-
 import SolutionInfoService from './solution-info.service';
 
 export default angular.module('SolutionInfoModule', [ElementsModule])
-    .component('appKnapsackSummary', KnapsackSummaryComponent)
     .component('appPopulationDetails', PopulationDetailsComponent)
     .service('SolutionInfoService', SolutionInfoService)
     .name;
