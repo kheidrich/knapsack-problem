56bbe10278d943610dcf88da5c4ce606c0f32056
Kevin <kevin.pinzon@ulbra.br>
2017-12-13 13:49:44 -0200

    modal footer fixed

--- src/ui/elements/modal/modal.component.js
+++ src/ui/elements/modal/modal.component.js
@@ -16,7 +16,8 @@ export default {
     controller: ModalComponent,
     controllerAs: 'Modal',
     bindings: {
-        name: '@'
+        name: '@',
+        hideFooter: '@'
     },
     transclude: true
 }
